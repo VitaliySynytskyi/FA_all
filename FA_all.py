ef9a9c71e7a4386542959c0bb5154281f4a01624
Vitalii Sviderskyi <isac.medison@gmail.com>
2025-05-07 19:30:01 +0300

    [add] BC periodic as default

--- FA_all.py
+++ FA_all.py
@@ -942,7 +942,7 @@ layout1 = html.Div([
                                                     {"label": "periodic", "value": "periodic"},
                                                     {"label": "ordinary", "value": "ordinary"}
                                                 ],
-                                                value="no",
+                                                value="periodic",
                                                 style={"font-weight": "bold"}
                                             ),
                                     dbc.InputGroupText("Boundary Condition:")
